a75d5138506f6d2b0895e0d0f7e2a791301691c4
Adrian Silva <cr7adrian@hotmail.com>
2017-10-13 22:59:57 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml
    #	app/src/main/java/com/example/thehacker/whatsthat/SplashMainActivity.java
    #	app/src/main/res/layout/activity_splash_main.xml

--- app/src/main/java/com/example/thehacker/whatsthat/SplashMainActivity.java
+++ app/src/main/java/com/example/thehacker/whatsthat/SplashMainActivity.java
@@ -1,28 +1,77 @@
 package com.example.thehacker.whatsthat;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.RotateAnimation;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import jp.wasabeef.blurry.Blurry;
 
 public class SplashMainActivity extends AppCompatActivity {
 
-    private Button mainButton;
+    private ImageView image;
+    private ImageView logoImage;
+    private ImageButton button;
+    private Animation translateAnimation;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_main);
 
-        mainButton = (Button)findViewById(R.id.mainButton);
+        image = (ImageView)findViewById(R.id.BackgroundView);
+        button = (ImageButton)findViewById(R.id.LogoImageButton);
 
-        mainButton.setOnClickListener(new View.OnClickListener() {
+
+        image.post(new Runnable() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(SplashMainActivity.this, NavigationActivity.class);
-                startActivity(intent);
+            public void run() {
+                setImageBlur();
+
             }
         });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startAnimation();
+                Intent intent = new Intent(SplashMainActivity.this, NavigationActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
+
+    private void startAnimation() {
+
+        button.setScaleX(0);
+        button.setScaleY(0);
+        button.animate().scaleX(1.5f).scaleY(1.5f);
+
+    }
+
+    private void setImageBlur() {
+        Blurry.with(SplashMainActivity.this)
+                .async()
+                .sampling(1)
+                .color(Color.argb(60, 0, 255, 255))
+                .capture(findViewById(R.id.BackgroundView))
+                .into((ImageView)findViewById(R.id.BackgroundView));
+
+    }
+
+
 }
